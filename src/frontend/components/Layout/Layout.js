0131d914487574feb5b04c706d37f3654a0464c9
CuerpoCasa <gabialberini733@gmail.com>
2022-06-02 13:08:39 -0300

    V3.4

--- src/frontend/components/Layout/Layout.js
+++ src/frontend/components/Layout/Layout.js
@@ -2,13 +2,16 @@
 import './Layout.css';
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
+import { ScrollButton } from '../BotonIrArriba/BotonIrArriba';
 
 const Layout = ({children}) => {
+  
   return (
     <>
         <Header />
         <main id="main-content">
           {children}
+          <ScrollButton />
         </main>
         <Footer />
     </>
