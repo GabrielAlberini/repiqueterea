57f2eed02caa272667722220e479b55e2d5a4176
CuerpoCasa <gabialberini733@gmail.com>
2022-06-08 18:29:32 -0300

    V5.4

--- src/frontend/services/dataItemsWork.js
+++ src/frontend/services/dataItemsWork.js
@@ -9,42 +9,42 @@ import {
 
 const ITEMS_WORKS = [
   {
-    img: "https://i.ibb.co/hgVhQ5L/cris-miro-Ilustracion.png",
+    img: "https://i.ibb.co/FJNyFsm/ilustraciones.png",
     alt: "Imagen de ilustración",
     title: "ILUSTRACIONES",
     description: "Transformo tu idea en dibujitos",
     route: ILUSTRACIONES
   },
   {
-    img: "https://i.ibb.co/DkfwP6t/morel-Mascotas.png",
+    img: "https://i.ibb.co/yyZKk87/mascotas.png",
     alt: "Imagen de ilustración",
     title: "MASCOTAS",
     description: "¿Quién no quiere un dibujito de su michi o perrito?",
     route: MASCOTAS
   },
   {
-    img: "https://i.ibb.co/8NwDZzC/barba-Personajes.png",
+    img: "https://i.ibb.co/WKJbHvX/personajes.png",
     alt: "Imagen de ilustración",
     title: "PERSONAJES",
     description: "Desde Ricardo Fort a Gilda, ¡desafiame!",
     route: PERSONAJES
   },
   {
-    img: "https://i.ibb.co/ZcTYd2X/dibujo-Blog.png",
+    img: "https://i.ibb.co/kBQCrK3/blog.png",
     alt: "Imagen de ilustración",
     title: "BLOG PERSONAL",
     description: "Un espacio para compartir experiencias",
     route: BLOG_PERSONAL
   },
   {
-    img: "https://i.ibb.co/p26HFS3/giuli-feria.jpg",
+    img: "https://i.ibb.co/MsJZWdG/expo.png",
     alt: "Imagen de ilustración",
     title: "EXPO / FERIAS",
     description: "También me podes encontrar en ferias y eventos",
     route: EXPO_FERIAS
   },
   {
-    img: "https://i.ibb.co/dpkm73Q/ricky-tienda.jpg",
+    img: "https://i.ibb.co/9ZvDcr4/tienda.png",
     alt: "Imagen de ilustración",
     title: "TIENDA",
     description: "¿Querés quedar de maravillas con un regalito?",
